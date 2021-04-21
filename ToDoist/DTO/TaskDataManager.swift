cd48c276d47d102ca13f24c458ee794e4bf442c4
micromirme <zaharov.v@outlook.com>
2021-04-21 12:47:20 +0300

    Completed with merge

--- ToDoist/DTO/TaskDataManager.swift
+++ ToDoist/DTO/TaskDataManager.swift
@@ -10,4 +10,5 @@ protocol TaskDataManager {
     func deleteTask(task: Task)
     func getTask(task: Task) -> Task?
     func getTotalCount() -> Int
+    func getTask(by status: Status) -> [Task]
 }
